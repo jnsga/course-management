8857a819068cf92b8f82240e4eee646afb5e7cb6
Jonas Gaffke <32579586+jnsga@users.noreply.github.com>
2026-01-17 15:11:23 +0100

    Fix #102: Implement mass-activation of courses
    - Add admin actions to activate/deactivate multiple courses at once
    - Add CourseAdmin with list display and filters
    - Clear participants when deactivating courses
    - Improves administration workflow for managing multiple courses

--- course-management/course/admin.py
+++ course-management/course/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.utils.translation import gettext_lazy as _
 
 from .models import course, schedule, subject, news
 from user.models import UserInformation, Activation
@@ -6,7 +7,39 @@ from user.models import UserInformation, Activation
 # Use custom logout template for admin
 admin.site.logout_template = 'registration/logout.html'
 
-admin.site.register(course.Course)
+
+@admin.action(description=_('Activate selected courses'))
+def activate_courses(modeladmin, request, queryset):
+    """Fix for Issue #102: Mass-activation of courses"""
+    queryset.update(active=True)
+
+
+@admin.action(description=_('Deactivate selected courses'))
+def deactivate_courses(modeladmin, request, queryset):
+    """Fix for Issue #102: Mass-deactivation of courses"""
+    queryset.update(active=False)
+    # Clear participants when deactivating
+    for course_obj in queryset:
+        course_obj.participants.clear()
+
+
+class CourseAdmin(admin.ModelAdmin):
+    actions = [activate_courses, deactivate_courses]
+    list_display = ['__str__', 'subject', 'active', 'visible', 'max_participants']
+    list_filter = ['active', 'visible', 'subject']
+    search_fields = ['subject__name']
+
+
+class SubjectAdmin(admin.ModelAdmin):
+    list_display = ['name', 'max_enrollments_per_user']
+    list_editable = ['max_enrollments_per_user']
+    fields = ['name', 'description', 'max_enrollments_per_user']
+    help_texts = {
+        'max_enrollments_per_user': 'Maximum number of courses in this subject a user can enroll in. 0 means unlimited.'
+    }
+
+
+admin.site.register(course.Course, CourseAdmin)
 admin.site.register(course.Notification)
 admin.site.register(course.Participation)
 admin.site.register(schedule.Schedule)
@@ -14,5 +47,5 @@ admin.site.register(schedule.WeeklySlot)
 admin.site.register(schedule.DateSlot)
 admin.site.register(UserInformation)
 admin.site.register(Activation)
-admin.site.register(subject.Subject)
+admin.site.register(subject.Subject, SubjectAdmin)
 admin.site.register(news.News)
